b1055e95050406b1c829bdef17ca7231c0f7a605
placek123pl <placeuszxd@gmail.com>
2025-10-29 20:21:07 +0100

    Klasa Person, Employee, Client, Volunteer

---
+++ src/Employee.java
@@ -0,0 +1,17 @@
+public class Employee extends Person{
+    private double salary;
+
+    public Employee(String name, String surname, int age, double salary) {
+        super(name, surname, age); // korzystanie z kontruktora klasy Person:)
+        this.salary = salary;
+    }
+
+// gettery i settery
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
