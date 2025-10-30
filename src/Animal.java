b06f5999e3c6f3af61233ae1906655f6a67595ca
Ender <tomasz.b.mlynarczyk@gmail.com>
2025-10-30 19:41:06 +0100

    Dodanie komentarzy do klasy Animal

--- src/Animal.java
+++ src/Animal.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;
 
+/**
+* Klasa Animal
+* Klasa abstrakcyjna mająca na celu być klasą po której będą dziedziczyć klasy gatunków zwierząt
+**/
 public abstract class Animal {
-    private static int lastId;
-    private final int id;
-    private int age;
-    private String name;
-    private ArrayList<String> pastNames = new ArrayList<String>();
-    private Traits trait;
-    //private Client adoptedBy;
+    private static int lastId; //Pole statyczne, zawierające ostatnie ID które zostało przypisane do ostatniego stworzonego obiektu klasy Animal
+    private final int id; //Pole typu final, nie może zostać nigdy zmienione, jest to unikalne ID przypisane do obiektu klasy Animal
+    private int age; //Pole age, zawiera wiek zwierzęcia
+    private String name; //Pole name, zawiera imię zwierzęcia
+    private ArrayList<String> pastNames = new ArrayList<String>(); //Lista String, jest to lista zawierająca historię imion zwierzęcia
+    private Traits trait; //Pole trait, zawiera jeden trait zwierzęcia
+    private Client adoptedBy; //Pole adoptedBy, zawiera informację przez kogo jest adoptowane dane zwierze
 
 
+    /**
+    * Konstruktor klasy Animal
+    * Wymaga podania name, age oraz trait, przypisuje podane dane do odpowiednich pól
+    **/
     Animal(String name, int age, Traits trait){
         this.age = age;
         this.name = name;
@@ -17,8 +25,10 @@ public abstract class Animal {
         this.id = lastId+=1;
     }
 
+    //Metoda przeliczająca wiek zwierzęcia na wiek ludzki, do implementacji w klasach dziedziczących
     abstract int calculateAgeInHumanYears();
 
+//Gettery i settery pól klasy
     public int getId() {
         return id;
     }
@@ -44,17 +54,18 @@ public abstract class Animal {
         return pastNames;
     }
 
-    void addPastName(String name) {
-        if (!this.pastNames.contains(name)) {
-            this.getPastNames().add(name);
-        }
-    }
-
     public Traits getTrait() {
         return trait;
     }
 
     public void setTrait(Traits trait) {
         this.trait = trait;
+    }
+
+    //Metoda dodająca podane imię do historii imion o ile dane imie już w niej nie jest
+    void addPastName(String name) {
+        if (!this.pastNames.contains(name)) {
+            this.getPastNames().add(name);
+        }
     }
 }
